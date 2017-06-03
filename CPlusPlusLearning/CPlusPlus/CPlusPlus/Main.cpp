1c3e87719c95a2fca43164cf7163c76148ab53d4
JonathanDechelle <jonathan.dechelle@hotmail.com>
2017-06-03 10:09:25 -0400

    ~ refactored Exercice class

--- CPlusPlusLearning/CPlusPlus/CPlusPlus/Main.cpp
+++ CPlusPlusLearning/CPlusPlus/CPlusPlus/Main.cpp
@@ -1,36 +1,5 @@
-#include "InputOutput.h"
-
-void GuessMyNumberExercice()
-{
-	int answer = 7;
-	int guess = 0;
-
-	bool foundGuess = false;
-	while(!foundGuess)
-	{
-		DisplayMessage("Guess the number : ");
-		guess = InputMessage(guess); 
-
-		if(guess > answer)
-		{
-			DisplayMessage("The number is too high", true);
-		}
-		else if(guess < answer)
-		{
-			DisplayMessage("The number is too low", true);
-		}
-		else
-		{
-			DisplayMessage("The number is correct", true);
-			foundGuess = true;
-		}
-	}
-
-	guess = InputMessage(guess);
-}
 
 int main()
 {
-	GuessMyNumberExercice();
 	return 0;
 }
