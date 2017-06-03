d36ab55b0e71472134cf3ba2b55de3fa427ecb24
JonathanDechelle <jonathan.dechelle@hotmail.com>
2017-06-03 09:22:28 -0400

    ~ Guess my number exercice

--- CPlusPlusLearning/CPlusPlus/CPlusPlus/Main.cpp
+++ CPlusPlusLearning/CPlusPlus/CPlusPlus/Main.cpp
@@ -3,8 +3,30 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello World";
-	cout << endl;
-	system("pause");
+	int answer = 7;
+	int guess;
+
+	bool foundGuess = false;
+	while(!foundGuess)
+	{
+		cout << "Guess the number : ";
+		cin >> guess;
+
+		if(guess > 7)
+		{
+			cout << "The number is too high" << endl;;
+		}
+		else if(guess < 7)
+		{
+			cout << "The number is too low" << endl;;
+		}
+		else
+		{
+			cout << "The number is correct" << endl;;
+			foundGuess = true;
+		}
+	}
+
+	cin >> guess;
 	return 0;
 }
